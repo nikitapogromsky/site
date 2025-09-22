bfdb62789605bd9d9881f1a19a4b3a74990911ca
Pogromskiy Nikita <129324283+nikitapogromsky@users.noreply.github.com>
2025-09-22 15:38:10 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ TEMPLATE = """
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>web-site</title>
+    <title> web-site </title>
     <style>
         body {
             font-family: Arial, sans-serif;
